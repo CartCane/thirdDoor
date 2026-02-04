71e73bfb0bc0932d51653e126be92170b2d7a400
Karthikeyan <raghupathykartik@gmail.com>
2026-02-04 15:20:02 +0530

    change of address

--- src/components/footer/ContactInfo.jsx
+++ src/components/footer/ContactInfo.jsx
@@ -9,7 +9,7 @@ export default function ContactInfo() {
       <ContactItem header="8088723265" icon={<Phone />} description="Mon-Sat: 8:00 - 21:00"/>
       <ContactItem header="connect@thirddoor.com" icon={<Mail />} description="24/7 customer support"/>
       <ContactItem 
-        header="627 - 628, 4th Floor, 5th Cross, 15th Main, 80 Feet Rd, Koramangala 4th Block, Bengaluru, Karnataka 560034"
+        header="114/5, Old Madras Road, Halasuru, Hoysala Nagar, Bengaluru, Karnataka 560008"
         icon={<Building />}
         description="Main office location"
       />
